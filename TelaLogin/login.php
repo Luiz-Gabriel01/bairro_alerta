36cfd8d6a4016691c76946c4c8edb2b64fb02899
Luiz-Gabriel01 <luiz.gabriel12br@gmail.com>
2025-05-15 23:27:32 -0300

    alteracoes baiscas

--- TelaLogin/login.php
+++ TelaLogin/login.php
@@ -6,20 +6,20 @@ if (!$conexao) {
     die("Erro: Não foi possível conectar ao banco de dados.");
 }
 
-// Receber os dados do formulário
+
 $email = $_POST['email'];
 $senha = md5($_POST['senha']); // Senha criptografada
 
-// Consultar o banco de dados
+
 $query = "SELECT * FROM usuario WHERE email = '$email' AND senha = '$senha'";
 $result = mysqli_query($conexao, $query);
 
 if (mysqli_num_rows($result) > 0) {
-    // Login bem-sucedido: Redirecionar para a página home
+    
     header("Location: ../Telanavegacao/navegacao.html");
     exit();
 } else {
-    // Login falhou: Mostrar erro e voltar à tela de login
+    
     echo "<script>
             alert('Erro: Email ou senha inválidos.');
             window.history.back();
